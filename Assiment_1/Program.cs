b15f2fc3a91f65190c316ba5c5c70b99c3faa16b
Aboud Daboubash <Aboud Daboubash@DESKTOP-L54RJK8>
2026-02-11 13:14:01 +0300

    Add project files.

---
+++ Assiment_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Inter Bill Amount ");
+var billAmount = Convert.ToDouble(Console.ReadLine());
+var finalPrice = 0.0;
+
+
+if (billAmount >= 500)
+{
+    finalPrice = billAmount - (billAmount * 20 / 100);
+    Console.WriteLine(finalPrice);
+}
+else if (billAmount < 500 && billAmount >= 300)
+{
+    finalPrice = billAmount - (billAmount * 10 / 100);
+    Console.WriteLine(finalPrice);
+}
+else
+{
+    finalPrice = billAmount;
+    Console.WriteLine(finalPrice);
+}
